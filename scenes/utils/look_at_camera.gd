e09e6abab666c6a1ed1bf29b09e0d9d3469059ed
Torstein <60714820+TorsteinOvstedal@users.noreply.github.com>
2023-10-04 18:05:25 +0200

    Added camera zoom controls.

--- scenes/utils/look_at_camera.gd
+++ scenes/utils/look_at_camera.gd
@@ -8,20 +8,34 @@ var target: Node3D = null
 
 var requested := false
 
-var speed = 2
-var target_distance = 300
+# var speed = 2
+# var target_distance = 300
 
 func set_target(path: NodePath) -> void:
 	target = get_node_or_null(path)
 
+func _unhandled_input(event):
+	print(event)
+	
+	if event is InputEventMouseButton:
+		if event.button_index == 4:
+			var d = global_position.direction_to(target.global_position)
+			position += d * 4
+		elif event.button_index == 5:
+			var d = global_position.direction_to(target.global_position)
+			position -= d * 4
+	
 func _physics_process(delta: float) -> void:
 	if target and target.is_inside_tree():
 		look_at(target.global_position)
-		var distance = global_position.distance_squared_to(target.global_position)
-		if distance > target_distance:
-			position += global_position.direction_to(target.global_position) * speed * delta
-		elif distance < target_distance:
-			position -= global_position.direction_to(target.global_position) * speed * delta
+			
+		
+		# Stationary camera for now.
+		# var distance = global_position.distance_squared_to(target.global_position)
+		# if distance > target_distance:
+		# 	position += global_position.direction_to(target.global_position) * speed * delta
+		# elif distance < target_distance:
+		# 	position -= global_position.direction_to(target.global_position) * speed * delta
 		
 		requested = false
 
